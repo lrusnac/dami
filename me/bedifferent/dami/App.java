076022ed0a7b4af2fbc89de9587a41f6fa1dbf58
Leonid <leonidrusnac4@gmail.com>
2016-03-07 14:15:39 +0100

    something

--- me/bedifferent/dami/App.java
+++ me/bedifferent/dami/App.java
@@ -1,13 +1,18 @@
 package me.bedifferent.dami;
 
+import java.util.Arrays;
+
 public class App {
 
 	public static void main(String args[]) {
-		DataFrame df = new DataFrame();
+		int[] colums = {1, 2, 3, 4, 5, 6, 7, 20, 21, 44, 45};
+		Class[] types = {Integer.class, String.class, String.class, Double.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class};
+		String[] header = {"age", "gender", "shoesize", "height", "course", "interess", "languages", "playing", "games", "row", "seat"};
+		DataFrame df = new DataFrame(header, types, colums, "DataMining 2016 (Responses).csv");
+		System.out.println(df);
 		
-		boolean skipHeaderRow = false;
-		df.readFromCSV("DataMining 2016 (Responses).csv", ";", "-", skipHeaderRow);
+		System.out.println("ages: " + Arrays.toString(df.getColumn("age")));
 		
-		System.out.println(df.toString());
+		System.out.println("data frame size: " + df.getSize());
 	}
 }
--- me/bedifferent/dami/DataFrame.java
+++ me/bedifferent/dami/DataFrame.java
@@ -5,20 +5,40 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class DataFrame {
 	private Object[][] df;
+	private String[] header;
+	private Class[] types;
+	private int[] interestedColumns;
 	
-	public void readFromCSV(String csvFile, String seperator, String nullValue, boolean skipHeaderRow) {
+	String csvFile;
+	
+	public DataFrame(String[] header, Class[] types, int[] columns, String csvFile) {
+		if(header.length != types.length || types.length != columns.length) {
+			System.err.println("the arrays must have the same length");
+			return;
+		}
+		this.header = header;
+		this.interestedColumns = columns;
+		this.types = types;
+		this.csvFile = csvFile;
+		
+		this.readFromCSV(";", true);
+	}
+	
+	private void readFromCSV(String seperator, boolean hasHeader) {
 		List<Object[]> lines = new ArrayList<Object[]>();
 		BufferedReader bufRdr = null;
 		try {
 			bufRdr = new BufferedReader(new FileReader(new File(csvFile)));
 		} catch (FileNotFoundException e) {
-			System.out.println("No such file");
+			System.err.println("No such file");
 			e.printStackTrace();
 		}
 
@@ -26,35 +46,76 @@ public class DataFrame {
 
 		try {
 			while ((line = bufRdr.readLine()) != null) {
-				if (skipHeaderRow) {
-					skipHeaderRow = false;
+				if (hasHeader) {
+					hasHeader = false;
 					continue;
 				}
-
 				String[] arr = line.split(seperator); 
-
-				for(int i = 0; i < arr.length; i++){
-					if(arr[i].equals("")){
-						arr[i] = nullValue;
-					}				
+				
+				Object[] row = new Object[interestedColumns.length];
+				
+				for(int i=0; i<interestedColumns.length; i++) {
+					if(types[i].equals(Integer.class)) {
+						row[i] = Integer.parseInt(arr[interestedColumns[i]]);
+					} else if(types[i].equals(Double.class)) {
+						row[i] = Double.parseDouble(arr[interestedColumns[i]]);
+					} else if(types[i].equals(Boolean.class)) {
+						row[i] = Boolean.parseBoolean(arr[interestedColumns[i]]);
+					} else {
+						try {
+							row[i] = types[i].getConstructor(String.class).newInstance(arr[interestedColumns[i]]);
+						} catch (InstantiationException
+								| IllegalAccessException
+								| IllegalArgumentException
+								| InvocationTargetException
+								| NoSuchMethodException | SecurityException e) {
+							System.err.println("cannot cast to type");
+							e.printStackTrace();
+						}
+					}
+					
 				}
-
-				lines.add(arr);			
+				
+				lines.add(row);			
 			}
 			
 			Object[][] ret = new Object[lines.size()][];
 			bufRdr.close();
 			df = lines.toArray(ret);
 		} catch (IOException e) {
-			System.out.println("csv file parsing error");
+			System.err.println("csv file parsing error");
 			e.printStackTrace();
 		}
 	}
 	
+	public int getSize() {
+		return df.length;
+	}
 	
+	public Object[] getColumn(String name) {
+		int columNumber = headerNameToColNumber(name);
+		return getColumn(columNumber);
+	}
 	
+	public Object[] getColumn(int columNumber) {
+		Object[] col = new Object[getSize()];
+		
+		for(int i=0; i<col.length; i++) {
+			col[i] = df[i][columNumber];
+		}
+		
+		return col;
+	}
+	
+	private int headerNameToColNumber(String name) {
+		for(int i=0; i<header.length; i++) {
+			if(header[i].equals(name)) return i;
+		}
+		return -1;
+	}
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
+		sb.append(Arrays.toString(header)).append("\n");
 		for (Object[] line : df) {
 			sb.append(Arrays.toString(line)).append("\n");
 		}
